f31c8ecca2165277fe8d4b1300d7496b8c381450
橙续缘 <lixiaohui2019@sina.cn>
2019-11-06 16:34:44 +0800

    fix: 关闭接口安全功能

--- config/config.default.js
+++ config/config.default.js
@@ -5,9 +5,14 @@ module.exports = appInfo => {
   config.keys = `${appInfo.name}_1573021756112_3991`
   config.middleware = []
 
+  config.security = {
+    csrf: false
+  }
+
   config.toolbox = {
     appId: 'wxa3a1ed7e9913ca17',
     appSecret: '2747c603d324663f653b2da9f7d05258'
   }
+
   return config
 }
